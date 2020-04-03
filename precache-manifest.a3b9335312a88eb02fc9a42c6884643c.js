380ef4e69b67536dd9ec2729360a8ecd57fe4636
Leigh Silverstein <l2silver@ryerson.ca>
2020-04-03 14:49:40 -0400

    init commit

---
+++ precache-manifest.a3b9335312a88eb02fc9a42c6884643c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e63a89f442b60090a9679ab3cbcacd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "433453d837c293ebe024",
+    "url": "/static/css/main.b472f7f6.chunk.css"
+  },
+  {
+    "revision": "371f4fcedc07b13c770e",
+    "url": "/static/js/2.eddc4038.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.eddc4038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433453d837c293ebe024",
+    "url": "/static/js/main.d4952410.chunk.js"
+  },
+  {
+    "revision": "57591b0362576f22b5a5",
+    "url": "/static/js/runtime-main.4d58bb48.js"
+  }
+]);
